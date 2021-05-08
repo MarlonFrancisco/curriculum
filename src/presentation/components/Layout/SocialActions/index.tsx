4c4686fd3ea175e21b8a01611eb30202384d6499
Marlon Francisco <consultor5a.marlon@webmotors.com.br>
2021-05-07 22:00:36 -0300

    feat: improviments seo

--- src/presentation/components/Layout/SocialActions/index.tsx
+++ src/presentation/components/Layout/SocialActions/index.tsx
@@ -7,15 +7,17 @@ const SocialActions = () => {
     <S.ButtonActions>
       <S.LinkSocialNetwork
         target="_blank"
+        rel="noopener"
         href="https://www.linkedin.com/in/marlon-francisco-a8ab2215b/"
       >
-        <Image src="/images/linkedin.svg" />
+        <Image src="/images/linkedin.svg" alt="Linkedin Marlon Francisco" />
       </S.LinkSocialNetwork>
       <S.LinkSocialNetwork
         target="_blank"
+        rel="noopener"
         href="https://github.com/MarlonFrancisco"
       >
-        <Image src="/images/github.svg" />
+        <Image src="/images/github.svg" alt="Github Marlon Francisco" />
       </S.LinkSocialNetwork>
     </S.ButtonActions>
   );
